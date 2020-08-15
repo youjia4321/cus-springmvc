2a348324f05ef7b73cf7e43ab89b0593e3a74312
youjia <youjia4321@163.com>
2020-08-15 09:50:28 +0800

    将冗余代码抽取成属性 对控制器上参数HttpServletResponse HttpServletRequest进行赋值及对返回的类型做判断

--- src/main/java/com/springmvc/handler/HandlerExecutionChain.java
+++ src/main/java/com/springmvc/handler/HandlerExecutionChain.java
@@ -1,6 +1,6 @@
 package com.springmvc.handler;
 
-import com.alibaba.fastjson.JSON;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.springmvc.model.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
@@ -8,40 +8,42 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.util.Arrays;
 
 public class HandlerExecutionChain {
 
     public HandlerMapping handler;
+    private Method method;
+    private Object bean;
 
     public HandlerExecutionChain(HandlerMapping handler) {
         this.handler = handler;
+        this.method = handler.getMethod();
+        this.bean = handler.getController();
     }
 
-    public ModelAndView handle(HttpServletRequest req) throws InvocationTargetException, IllegalAccessException {
-
-        Method method = handler.getMethod();
-        Object bean = handler.getController();
-        // 获取参数
-        Object[] parameters = new Object[handler.getArgs().length];
-        for(int i = 0; i < handler.getArgs().length; i++) {
-            parameters[i] = req.getParameter(handler.getArgs()[i]);
-        }
-        Object viewName = method.invoke(bean, parameters);
-        return new ModelAndView((String) viewName);
-    }
-
-    public void handle(HttpServletRequest req, HttpServletResponse resp)
+    public ModelAndView handle(HttpServletRequest req, HttpServletResponse resp)
             throws InvocationTargetException, IllegalAccessException, IOException {
-        resp.setContentType("application/json;charset=utf-8");
-        Method method = handler.getMethod();
-        Object bean = handler.getController();
         // 获取参数
         Object[] parameters = new Object[handler.getArgs().length];
+        // System.out.println("当前控制器上的参数列表：" + Arrays.toString(handler.getArgs()));
         for(int i = 0; i < handler.getArgs().length; i++) {
-            parameters[i] = req.getParameter(handler.getArgs()[i]);
+            if("HttpServletRequest".equals(handler.getArgs()[i])){
+                parameters[i] = req;
+            } else if("HttpServletResponse".equals(handler.getArgs()[i])) {
+                parameters[i] = resp;
+            } else {
+                parameters[i] = req.getParameter(handler.getArgs()[i]);
+            }
         }
-        Object response =  method.invoke(bean, parameters);
-        resp.getWriter().println(JSON.toJSONString(response.toString()));
+        Object response = method.invoke(bean, parameters);
+        if(handler.isResponseBody()) {
+            resp.setContentType("application/json;charset=utf-8");
+            ObjectMapper objectMapper = new ObjectMapper();
+            resp.getWriter().print(objectMapper.writeValueAsString(response));
+            return null;
+        }
+        return new ModelAndView((String) response);
     }
 
 }

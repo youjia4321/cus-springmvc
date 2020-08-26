673c3d9debed00611d4bd6ceb5aa2b0b9fbb231c
youjia <youjia4321@163.com>
2020-08-26 11:56:18 +0800

    创建@Aspect @Around注解

---
+++ src/main/java/com/springmvc/annotation/Around.java
@@ -0,0 +1,12 @@
+package com.springmvc.annotation;
+
+import java.lang.annotation.*;
+
+@Target(ElementType.METHOD) // 元注解 定义范围
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface Around {
+
+    String value() default "";
+
+}

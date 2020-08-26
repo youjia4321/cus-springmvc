0dbc77ac6b89a178be6694202232f62a4f61dfa4
youjia <youjia4321@163.com>
2020-08-26 11:52:44 +0800

    实现测试抽象方法

--- src/main/java/com/example/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/service/impl/UserServiceImpl.java
@@ -5,13 +5,17 @@ import com.example.service.UserService;
 import com.springmvc.annotation.Service;
 
 @Service
-public class UserServiceImpl implements UserService {
+public class UserServiceImpl implements UserService{
     @Override
     public User findUser(String name) {
         if(name.equals("happy")) {
             return new User(1, "happy", 23, "123456");
-
         }
         return null;
+    }
+
+    @Override
+    public String show(String name) {
+        return name + "今天过情人节";
     }
 }
